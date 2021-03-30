548a1e852a34f2b68e01e08a69c477f6b8fe688b
set123ed <set123ed@gmail.com>
2021-03-30 02:19:49 -0400

    adding image

--- practica7/practica7/Views/PlatformSpecificPage.xaml.cs
+++ practica7/practica7/Views/PlatformSpecificPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-
-
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
@@ -13,32 +11,33 @@ namespace practica7
         public PlatformSpecificPage()
         {
             InitializeComponent();
-            var aboveButton = new Xamarin.Forms.Button { Text = "Button Above BoxView - Click Me" };
-            aboveButton.On<Android>().SetElevation(10);
 
-            Content = new StackLayout
-            {
-                Children =
-            {
-                new Grid
-                {
-                    Children =
-                    {
-                        new Xamarin.Forms.Button { Text = "Button Beneath BoxView" },
-                        new BoxView { Color = Color.Red, Opacity = 0.2, HeightRequest = 50 }
-                    }
-                },
-                new Grid
-                {
-                    Margin = new Thickness(0,20,0,0),
-                    Children =
-                    {
-                        aboveButton,
-                        new BoxView { Color = Color.Red, Opacity = 0.2, HeightRequest = 50 }
-                    }
-                }
-            }
-            };
+            //var aboveButton = new Xamarin.Forms.Button { Text = "Button Above BoxView - Click Me" };
+            //aboveButton.On<Android>().SetElevation(10);
+
+            //Content = new StackLayout
+            //{
+            //    Children =
+            //{
+            //    new Grid
+            //    {
+            //        Children =
+            //        {
+            //            new Xamarin.Forms.Button { Text = "Button Beneath BoxView" },
+            //            new BoxView { Color = Color.Yellow, Opacity = 0.2, HeightRequest = 50 }
+            //        }
+            //    },
+            //    new Grid
+            //    {
+            //        Margin = new Thickness(0,20,0,0),
+            //        Children =
+            //        {
+            //            aboveButton,
+            //            new BoxView { Color = Color.Red, Opacity = 0.2, HeightRequest = 50 }
+            //        }
+            //    }
+            //}
+            //};
         }
     }
 }
